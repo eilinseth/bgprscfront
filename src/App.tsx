f392e115fa5ab2bb119751cd65cb263e13ebd099
eilinseth <aksun989@gmail.com>
2025-08-20 15:29:09 +0700

    fix div

--- src/App.tsx
+++ src/App.tsx
@@ -111,9 +111,9 @@ function App() {
   }
   return (
     <div className="scroll-smooth ">
-    <div id="bg-main" className="  h-screen w-screen  ">
-      <div className="bg-white/20 backdrop-blur-sm w-screen h-screen ">
-      <section className="px-[6%] pt-20 md:pt-50 ">
+    <div id="bg-main" className="  min-h-screen w-screen  ">
+      <div className="bg-white/20 backdrop-blur-sm w-screen min-h-screen ">
+      <section className="px-[6%] pt-5 md:pt-50 ">
         <h1 className="font-bold text-white drop-shadow-black drop-shadow-xs text-4xl mt-20 md:-mt-14 ">{lang === "en" ? "BGPview ASN to MikroTik RSC" : "ASN BGPview ke RSC MikroTik"}</h1>
       </section>
 
@@ -136,7 +136,7 @@ function App() {
           </div>
       </div>
     </div>
-        <section className="min-h-screen w-screen bg-slate-300 pb-15 pt-30  md:px-20 " >
+        <section className="min-h-screen w-screen bg-slate-300 py-20 md:px-20 " >
           <h1 className="text-center mb-10 text-4xl bg-linear-90 md:mb-20 from-[#3B82F6] to-[#9333EA] bg-clip-text text-transparent font-bold">{lang === "en" ? "Generate Mikrotik Firewall Script" : "Membuat Script Firewall Mikrotik"}</h1>
 
           <div className="flex justify-center items-center flex-col gap-5">
@@ -206,62 +206,62 @@ function App() {
                 <li className="">
                     <p>{lang === "en" ?  "Get the ASN number from" : "Dapatkan nomor ASN dari"}<a target="_blank" href="https://bgpview.io/" className="font-bold"> bgpview.io</a></p>
                   <div className="flex flex-col md:flex-row ">
-                  <img src="/tutorial/1.png" className="w-100 lg:w-130 lg:h-70 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/2.png" className="w-100 lg:w-130 lg:h-70 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/1.png" className="w-100 lg:w-130 lg:h-70 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/2.png" className="w-100 lg:w-130 lg:h-70 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Fill the form with ASN Number, Address List name (decide naming of address-list in MikroTik), and File Name (decide the file name for downloaded)" : "Isi form dengan nomor ASN, Nama Address List (untuk penamaan address-list di MikroTik), dan Nama File (untuk penamaan nama file yang akan diunduh)"
                   }
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/3.png" className="w-100 lg:w-130 lg:h-90 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/5.png" className="w-100 lg:w-100 lg:h-90 mt-2 ml-4 md:ml-20" alt="" />
+                  <img src="/tutorial/3.png" className="w-100 lg:w-130 lg:h-90 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/5.png" className="w-100 lg:w-100 lg:h-90 mt-2 md:ml-4 md:ml-20" alt="" />
                   
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Download the file" : "Unduh file nya"}
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/4.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/6.png" className="w-100 lg:w-130 h-20 lg:h-30 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/4.png" className="w-100 lg:w-130 lg:h-80 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/6.png" className="w-100 lg:w-130 h-20 lg:h-30 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Drag the file into Files menu in MikroTik": "Seret/tarik file ke menu Files di MikroTik"
                   }
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/7.png" className="w-100 lg:w-130 lg:h-90 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/8.png" className="w-100 lg:w-130 h-40 lg:h-50 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/7.png" className="w-100 lg:w-130 lg:h-90 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/8.png" className="w-100 lg:w-130 h-40 lg:h-50 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                     <p>{lang === "en" ? "Write command " : "Tulis perintah "}"<span className="text-[#CC66FF]">import</span> <span className="text-[#66FF66]">file-name</span><span className="text-amber-300">=</span>facebook.rsc" {lang === "en" ? "in terminal" : "di terminal"}</p> 
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/9.png" className="w-100 lg:w-130 md:h-10 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/10.png" className="w-100 lg:w-130 lg:h-60 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/9.png" className="w-100 lg:w-130 md:h-10 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/10.png" className="w-100 lg:w-130 lg:h-60 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Check Address List" : "Cek Address List"}
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/11.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/11.1.png" className="w-100 lg:w-130 lg:h-80 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/11.png" className="w-100 lg:w-130 lg:h-80 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/11.1.png" className="w-100 lg:w-130 lg:h-80 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Create a firewall rule. Set In/Out Interface to the interface that is connected to the network, do the same thing with ipv6":"Buat sebuah rule firewall. Atur In/Out Interface dengan interface yang terkoneksi dengan jaringan internet, lakukan hal yang sama untuk ipv6"
                   }
                   <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap">
-                  <img src="/tutorial/12.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/13.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/14.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 ml-4" alt="" />
+                  <img src="/tutorial/12.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/13.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/14.png" className="w-100 lg:w-130 h-80 lg:h-80 mt-2 md:ml-4" alt="" />
                   </div>
                 </li>
                 <li className="mt-20">
                   {lang === "en" ? "Result" : "Hasil"}
                   <div className="flex flex-col md:flex-row">
-                  <img src="/tutorial/16.png" className="w-100 lg:w-130 md:h-10 mt-2 ml-4" alt="" />
-                  <img src="/tutorial/15.png" className="w-100 lg:w-130 lg:h-80 mt-1 ml-4" alt="" />
+                  <img src="/tutorial/16.png" className="w-100 lg:w-130 md:h-10 mt-2 md:ml-4" alt="" />
+                  <img src="/tutorial/15.png" className="w-100 lg:w-130 lg:h-80 mt-1 md:ml-4" alt="" />
                   </div>
                 </li>
               </ol>
